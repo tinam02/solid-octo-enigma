1dbe702a4176de518b9208a71ebc9a45b8a91746
Tina M <oge612@gmail.com>
2023-11-03 19:27:55 +0100

    1

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -7,12 +7,9 @@ interface NavProps {
 
 const Nav: React.FC<NavProps> = ({ hide = false }) => {
   return (
-    <nav className="flex items-center gap-4 m-5 p-4" style={{
-      background:'blue'
-    }}>
-      <div>a</div>   <div>a</div>
-     <Pill text="Home" className="secondary" />
-     <Pill text="Home" className="secondary" />
+    <nav className="flex items-center gap-4 px-4 navx" style={{}}>
+      <Pill text="Home" className="secondary" />
+      <Pill text="Home" className="secondary" />
     </nav>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { AnimatePresence, motion, Spring } from "framer-motion";
+import Layout from "@/components/Layout";
+import Nav from "@/components/Nav";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps,router }: AppProps) {
+    const transitionSpringPhysics: Spring = {
+    type: "spring",
+    mass: 0.2,
+    stiffness: 24,
+    damping: 10,
+  };
+  return (
+    <Layout title="Tina Milosavljevic">
+      <AnimatePresence mode="wait">
+        <motion.div key={router.route}>
+          <motion.div
+            className="blur"
+            transition={transitionSpringPhysics}
+            animate={{ opacity: 0 }}
+          />
+          <Nav />
+          <Component {...pageProps} />{" "}
+        </motion.div>
+      </AnimatePresence>
+    </Layout>
+  );
 }
