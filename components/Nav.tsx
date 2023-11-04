1164676be5213d92600181f433a2a1b5b101952f
Tina M <oge612@gmail.com>
2023-11-04 14:34:10 +0100

    nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -5,17 +5,34 @@ import classNames from 'classnames';
 import { RandomizeFont } from './atoms/FontRandomizer';
 import AnimatedTextCharacter from './atoms/AnimatedText';
 
+import { AnimatePresence, motion } from 'framer-motion';
+
 interface NavProps {
   hide?: boolean;
 }
 
 const Nav: React.FC<NavProps> = ({ hide = false }) => {
   const { fonts, toggleTheme } = useStyle();
+  const [open, setOpen] = useState(false);
+
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.15,
+      },
+    },
+  };
+  const item = {
+    hidden: { opacity: 0, x: 100 },
+    show: { opacity: 1, x: 0 },
+  };
 
   return (
-    <nav className='flex items-center gap-3 px-2 navx' style={{}}>
-      <Pill className={classNames('secondary', fonts.divine)}>Arch</Pill>
-      <div className='flex items-center gap-4'>
+    <nav className='flex items-center px-2 navx select-none' style={{}}>
+      <Pill className={classNames('secondary', fonts.divine)}>as </Pill>
+      <div className=' items-center gap-3 hidden md:flex'>
         <Pill className='primary'>Home</Pill>
         <Pill className='secondary'>Email</Pill>
         <Pill
@@ -25,10 +42,37 @@ const Nav: React.FC<NavProps> = ({ hide = false }) => {
           }}
         >
           <RandomizeFont text='LinkedIn' />
-         </Pill>
+        </Pill>
         <Pill className='secondary' onClick={toggleTheme}>
           T
         </Pill>
+      </div>
+      <div className='relative md:hidden'>
+        <Pill className='secondary' onClick={() => setOpen(prev => !prev)}>
+          Menu
+        </Pill>
+        <AnimatePresence>
+          {open && (
+            <motion.div
+              key='menu'
+              variants={container}
+              initial='hidden'
+              animate='show'
+              className='absolute top-[60px] right-0 flex flex-col items-end gap-4'
+            >
+              <motion.div variants={item}>
+                <Pill className='secondary' onClick={toggleTheme}>
+                  T
+                </Pill>
+              </motion.div>
+              <motion.div variants={item}>
+                <Pill className='secondary' onClick={toggleTheme}>
+                  Tsfsdfsdf
+                </Pill>
+              </motion.div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </nav>
   );
