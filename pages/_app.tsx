2594a6891c1a06855c16ebccf427261068a0bb94
Tina <oge612@gmail.com>
2025-03-22 00:53:22 +0100

    favi

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,30 @@
-import Layout from "@/components/Layout";
-import { StyleProvider } from "@/hooks/StyleProvider";
-import "@/styles/globals.css";
-import { AnimatePresence, motion, Spring } from "framer-motion";
-import type { AppProps } from "next/app";
+import Layout from '@/components/Layout';
+import { StyleProvider } from '@/hooks/StyleProvider';
+import '@/styles/globals.css';
+import { AnimatePresence, motion, Spring } from 'framer-motion';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps, router }: AppProps) {
   const transitionSpringPhysics: Spring = {
-    type: "spring",
+    type: 'spring',
     mass: 0.2,
     stiffness: 24,
     damping: 10,
   };
   return (
-    <StyleProvider>   <Layout title="T">
-      <AnimatePresence mode="wait">
-     
+    <StyleProvider>
+      <Layout title='Tina Milosavljevic'>
+        <AnimatePresence mode='wait'>
           <motion.div key={router.route}>
             <motion.div
-              className="blur"
+              className='blur'
               transition={transitionSpringPhysics}
               animate={{ opacity: 0 }}
             />
             <Component {...pageProps} />
           </motion.div>
-       
-      </AnimatePresence> </Layout>
+        </AnimatePresence>
+      </Layout>
     </StyleProvider>
   );
 }
